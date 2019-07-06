604799628aa97b969f652001743b151df786c584
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-07-06 20:43:32 -0300

    class Legal Person

---
+++ scripts/Legal_Person.js
@@ -0,0 +1,13 @@
+import {Person} from "./Person.js";
+
+export class Legal_Person extends Person{
+
+	constructor(name, email, phone, cnpj){
+		super(name, email, phone);
+		this.cnpj = cnpj;
+	}
+
+	createPerson(){
+		alert("Legal entity created");
+	}
+} 
