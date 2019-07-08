1411eae8f70e94af49341ddeff5a7add033226cc
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-07-07 21:13:58 -0300

    I added the method of printing the data of the created person.

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,6 @@
 import { Legal_Person } from "./Legal_Person.js";
 import { Physical_Person } from "./Physical_Person.js";
 
-function Legal(name, email, phone, id){
-	var legal = new Legal_Person(name, email, phone, id);
-}
-
-function Physical(name, email, phone, id){
-	var physical = new Physical_Person(name, email, phone, id);
-}
-
 function register(){
 	var name = document.getElementById("name").value;
 	var email = document.getElementById("email").value;
@@ -17,14 +9,33 @@ function register(){
 
 	var option = document.getElementById("type_person").value;
 
-	if(option == "CNPJ:"){
-		console.log("cnpj");
-		Legal(name, email, phone, id);
+	if(option == "CNPJ: "){
+		var legal = new Legal_Person(name, email, phone, id);
+		createDiv(legal, option);
 	} 
 	else{
-		console.log("cpf");
-		Physical(name, email, phone, id);
+		var physical = new Physical_Person(name, email, phone, id);
+		createDiv(physical, option);
 	}
+}
+
+/*  
+*	Este metodo ainda nao esta finalizado
+*	Tenho que dar uma refinada na ideia de impressao dos dados.
+*/
+
+function createDiv(person, type_person){ 
+	var div = document.createElement("div");
+	div.setAttribute("id", type_person);
+
+	var text = "\r\n" + "Name: " + person.getName()+
+		"\r\n " + " Email: "+ person.getEmail()+
+		"\r\n" + " Phone: "+ person.getPhone()+
+		type_person+ person.getId()
+	;
+
+	div = document.createTextNode(text);
+	document.body.appendChild(div);
 }
 
 window.register = register
